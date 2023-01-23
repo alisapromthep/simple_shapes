82a9ecbf8028e82ca94bd213026973c54a7b9603
Alisa Promthep <alisa.promthep@gmail.com>
2023-01-22 20:52:39 -0500

    shapes bouncing on a white plane

--- src/script.js
+++ src/script.js
@@ -13,20 +13,32 @@ const scene = new THREE.Scene()
 
 // Object
 const cube = new THREE.BoxGeometry(1, 1, 1);
-const cone = new THREE.ConeGeometry(1,1,10);
-const ball = new THREE.SphereGeometry(1,10,10)
+const cone = new THREE.ConeGeometry(1,1,3);
+const ball = new THREE.SphereGeometry(0.9,10,10)
 
 //each set is a vertex 
 
-const material = new THREE.MeshBasicMaterial({ color: 0xedede9 });
-const mesh = new THREE.Mesh(cube, material);
-mesh.position.x = -3;
-const coneMesh = new THREE.Mesh(cone, material);
-const ballMesh = new THREE.Mesh(ball, material);
-ballMesh.position.x = 3;
-scene.add(mesh);
-scene.add(coneMesh);
-scene.add(ballMesh);
+const redMaterial = new THREE.MeshBasicMaterial({ color: 0xd90429 });
+const blueMaterial = new THREE.MeshBasicMaterial({color: 0x3f37c9});
+const greenMaterial = new THREE.MeshBasicMaterial({color: 0x0a9396});
+const cubeMesh = new THREE.Mesh(cube, redMaterial);
+cubeMesh.position.x = -2;
+const coneMesh = new THREE.Mesh(cone, blueMaterial);
+const ballMesh = new THREE.Mesh(ball, greenMaterial);
+ballMesh.position.x = 2;
+
+
+const group = new THREE.Group();
+scene.add(group);
+group.add(cubeMesh, coneMesh,ballMesh)
+
+const floor = new THREE.PlaneGeometry(6,5);
+const floorMaterial = new THREE.MeshBasicMaterial({color: 0xe5e5e5})
+const floorMesh = new THREE.Mesh(floor, floorMaterial);
+floorMesh.rotation.x = - Math.PI/2;
+floorMesh.position.set = (0,0);
+
+scene.add(floorMesh)
 
 // Sizes
 const sizes = {
@@ -51,7 +63,8 @@ window.addEventListener('resize', () =>
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+camera.position.z = 5;
+camera.position.y = 2;
 scene.add(camera)
 
 // Controls
@@ -72,6 +85,13 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    const sinTime = Math.sin(elapsedTime)+2;
+    const bounceTime = sinTime > 0 ? sinTime: sinTime*-1;
+
+    //animation, bouncing 
+    cubeMesh.position.y = bounceTime * 1/4;
+    coneMesh.position.y = bounceTime * 1/3;
+    ballMesh.position.y = bounceTime * 1/2;
     // Update controls
     controls.update()
 
